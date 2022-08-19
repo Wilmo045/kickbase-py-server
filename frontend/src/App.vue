020f9884464ffceb1f49c3bde5972def62e93146
Wilmo <kaiserreich1871@outlook.de>
2022-08-19 22:39:01 +0200

    set up vue&bulma

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navigation from './components/Navigation.vue';
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <Navigation />
+      </nav>
+    </div>
+  </header>
+</template>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '@/components/Navigation.vue'
+</script>
+
+<template>
+  <main>
+    <Navigation />
+  </main>
+</template>
